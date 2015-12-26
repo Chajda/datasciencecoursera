6aaf438b82021f49239c7890c22411fa8a5a8c57
Ivo Chaloupka <chaloupka.ivo@gmail.com>
2015-12-26 01:13:39 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,19 +3,32 @@
 library(shiny)
 # Define UI for application
 shinyUI(pageWithSidebar(
-  headerPanel("Illuminate application"),
+  headerPanel("R Function grep"),
   sidebarPanel(
+    helpText("Search for matches to argument pattern within each element of a character vector."),
     h3("Inputs:"),
-    textInput("name", "What's your name?", ""),
-    sliderInput("age", "Your Age?", min = 0, max = 140, value = 0),
-    checkboxGroupInput("multiplicator", "Multiplicator?", c("100x"="100", "10x"="10", "0.1x"="0.1"), 0),
-    radioButtons("radioBox", "RadioBox", c("Male"="0", "Female"="1"))
+    textInput("pattern", "pattern", ""),
+    helpText("pattern = character string containing a regular expression (or character string for fixed = TRUE) to be matched in the given character vector."),
+    textInput("x", "x", ""),
+    helpText("x = a character vector where matches are sought."),
+    checkboxGroupInput("choices", "Choices", c(
+      "ignore.case"="i",
+      "perl"="p",
+      "value"="v",
+      "fixed"="f",
+      "useBytes"="u")),
+    helpText("ignore.case = if FALSE, the pattern matching is case sensitive and if TRUE, case is ignored during matching."),
+    helpText("perl = Should Perl-compatible regexps be used?"),
+    helpText("value = if FALSE, a vector containing the (integer) indices of the matches determined by grep is returned, and if TRUE, a vector containing the matching elements themselves is returned."),
+    helpText("fixed =  If TRUE, pattern is a string to be matched as is. Overrides all conflicting arguments."),
+    helpText("useBytes = If TRUE the matching is done byte-by-byte rather than character-by-character."),
+    radioButtons("invert", "invert", c("FALSE"="0", "TRUE"="1")),
+    helpText(" If TRUE return indices or values for elements that do not match."),
+    sliderInput("age", "Your Age?", min = 0, max = 140, value = 0)
   ),
   mainPanel(
     h3("Outputs:"),
-    helpText('Greatings'),
-    textOutput("text1"),
-    helpText('Your illuminaters numerous'),
-    textOutput("text2")
+    helpText('Result:'),
+    textOutput("text1")
   )
 ))
